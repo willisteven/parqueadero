e21716c1a4b21931c71859e6e76ea726b26c427c
William Steven Clavijo Valero - Ceiba Software <william.clavijo@ceiba.com.co>
2019-01-08 22:34:17 -0500

    cambios arch gradle

--- src/test/java/com/ceiba/parqueadero/unitarias/VigilanteTest.java
+++ src/test/java/com/ceiba/parqueadero/unitarias/VigilanteTest.java
@@ -3,6 +3,7 @@ package com.ceiba.parqueadero.unitarias;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,6 +14,7 @@ import com.ceiba.parqueadero.reglas.ReglasParqueadero2;
 import org.mockito.Mock;
 
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 //@TestPropertySource(locations = "classpath:test.properties")
 public class VigilanteTest {
